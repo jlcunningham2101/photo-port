90692fc8e7334e8e0bb2f20341e4b1f06857c987
Jill Cunningham <jleighcunningham@gmail.com>
2023-01-22 18:05:08 -0500

    completed tests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = document.getElementById("root");
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  root
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
